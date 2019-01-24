5d287d0c4682284c7ac45f8061bc46eae6c213d5
LieRossi <40723203+LieRossi@users.noreply.github.com>
2019-01-24 17:27:08 -0200

    Joystick corrigido

---
+++ ElieteRossi_UC7_A1_AlienCity_Mobile/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+
+	public float MoveSpeed;
+	public float RotationSpeed;
+	CharacterController cc;
+	private Animator anim;
+	protected Vector3 gravidade = Vector3.zero;
+	protected Vector3 move = Vector3.zero;
+	private bool jump = false;
+
+    public float moveSpeed = 8f;
+    public Joystick joystick;
+
+
+    void Start()
+	{
+		cc = GetComponent<CharacterController> ();
+		anim = GetComponent<Animator>();
+		anim.SetTrigger("Parado");
+	}
+
+    void Update()
+    {
+        Vector3 move = Input.GetAxis("Vertical") * transform.TransformDirection(Vector3.forward) * MoveSpeed;
+        transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * RotationSpeed * Time.deltaTime, 0));
+
+        if (!cc.isGrounded) {
+            gravidade += Physics.gravity * Time.deltaTime;
+        }
+        else
+        {
+            gravidade = Vector3.zero;
+            if (jump)
+            {
+                gravidade.y = 6f;
+                jump = false;
+            }
+        }
+        move += gravidade;
+        cc.Move(move * Time.deltaTime);
+        Anima();
+    
+
+         Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.forward * joystick.Vertical);
+
+        if (moveVector != Vector3.zero)
+        {
+            //transform.rotation = Quaternion.LookRotation(moveVector);
+            transform.Translate(moveVector * moveSpeed * Time.deltaTime, Space.World);
+        }
+}
+
+void Anima()
+	{
+		if (!Input.anyKey)
+		{
+			anim.SetTrigger("Parado");
+		} 
+		else 
+		{
+			if(Input.GetKeyDown("space"))
+			{
+				anim.SetTrigger("Pula");
+				jump = true;
+			}
+			else
+			{
+				anim.SetTrigger("Corre");
+			}
+		}
+	}
+}
